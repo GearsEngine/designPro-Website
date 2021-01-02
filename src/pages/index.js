2fa0fa0dc4d52d9cfe6db9053be8aaefaa8ed03a
GearsEngine <76562468+GearsEngine@users.noreply.github.com>
2021-01-02 14:47:54 -0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Span } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Text
+				font="--base"
+				text-transform="uppercase"
+				letter-spacing="1px"
+				max-width="850px"
+				color="--grey"
+				margin="0px"
+			>
+				designPro
+			</Text>
+			<Text
+				as="div"
+				margin="8px 0px 32px"
+				font="--headline1"
+				md-font="--headline2"
+				color="--dark"
+				max-width="850px"
+			>
+				<Span font-weight="normal">
+					designPro. It's
+				</Span>
+			</Text>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff0f554d97277001a086750",
+				"5ff0f554d97277001a086752"
+			]
+		},
+		"5ff0f554d97277001a086750": {
+			"id": "5ff0f554d97277001a086750",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff0f554d97277001a086752": {
+			"id": "5ff0f554d97277001a086752",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
